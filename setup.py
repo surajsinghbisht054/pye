f6b0307a91ca5a8b126b28d69c75591cdec8e423
hguandl <hguandl@gmail.com>
2018-12-04 01:58:57 +0800

    add requirement for python3 in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(name='raw_python',
       version='2018.12.04',
       description='Python Package For Raw Packets Programming',
+      python_requires='>3.0',
       license='Apache-2.0',
       url='https://github.com/lightsing/raw_python',
       author='Lightsing',
